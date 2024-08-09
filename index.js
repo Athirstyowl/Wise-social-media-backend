a8ee3002107b714673fbc61c27ceb1c6c48f7805
atharva <atharvagholap24@gmail.com>
2024-08-09 23:53:35 +0530

    cors-test

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: 'http://localhost:3001',
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:3001'], // Add other ports if necessary
   methods: 'GET,POST,PUT,DELETE',
-}));
+  credentials: true
+}
+app.use(cors(corsOptions));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
